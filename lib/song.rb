152cc8a3ccb29f08822634bee63a22c097f31ce7
github username <github email address>
2017-10-01 15:06:24 -0400

    done

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,10 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
-class Song
 
+#brooke is done
+
+class Song
 
   def self.table_name
     self.to_s.downcase.pluralize
@@ -59,6 +61,3 @@ class Song
   end
 
 end
-
-
-
